0f4ec070123b5011ed8d103b6c41821adfc23188
Elijah Prince <elijahprince73@gmail.com>
2017-08-28 00:15:02 -0700

    removed mustache

--- data2.js
+++ data2.js
@@ -1,31 +1,17 @@
-$(document).ready(function(){
-var listings = data.results.map(function(listing){
+var listings  =  data.results.map(function (listing) {
   return {
-    "title":listing.title,
-    "shop_name": listing.shop_name,
-    "price": listing.price,
-    "currency_code":listing.currency_code,
-    "url_75x75": listing.Images[0].url_75x75
-  };
+    'title':listing.title,
+    'price':listing.price,
+    'currency_code':listing.currency_code,
+    'url_75x75':listing.Images[0].url_75x75
+  }
+})
 
-});
+var listingData= {
+  "listing":listings
+}
 
-
-var listingData = {
-  "listings": listings
-};
-
-
-  var images = $("#images").text();
-  var listHtml = Mustache.render(images, listingData);
-  $("#boxes2").html(listHtml);
-});
-
-
-
-
-
-
-
-
-
+listingData.listing.forEach(function (listItem) {
+document.getElementById('content').innerHTML = listItem.title
+console.log(listItem);
+})
